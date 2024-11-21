b9d3dc7b8ef13b44e9bb64b910c75c081e1a97ad
Duy18ND <duy18namdinh@gmail.com>
2024-11-22 01:25:12 +0700

    Add files via upload

---
+++ Sesion03-07.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	//khai bao numb1, numb2, sum
+	int numb1, numb2, sum; 
+	//nhap gia tri numb1, numb2 
+	printf("ban can nhap so numb1: ");
+	scanf("%d",&numb1);
+	printf("ban can nhap so num 2: ");
+	scanf("%d",&numb2);
+	//tinh sum 
+	 sum = numb1 + numb2;
+	 // in ra màn hinh
+	 printf("tong cong la: %d + %d = %d",numb1,numb2,sum);
+	  
+	return 0; 
+} 
