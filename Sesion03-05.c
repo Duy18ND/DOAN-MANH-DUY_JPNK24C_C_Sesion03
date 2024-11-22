0f2f9e5c1c0dfdf3d3d5a20881ba35d988399527
Duy18ND <duy18namdinh@gmail.com>
2024-11-22 10:46:56 +0700

    Add files via upload

--- Sesion03-05.c
+++ Sesion03-05.c
@@ -1,28 +1,23 @@
+#include<stdio.h>
+int main(){
+	printf("					DANH SACH SINH VIEN \n");
+	printf("------------------------------------------------------------------------");
+	printf("| STT |	  HO VA TEN	   | TUOI  | SO DIEN THOAI |	      EMAIL           | \n 	");
+	printf("|  1  | Doan Manh Duy  |   18  | 0336620883    | duy18namdinh@gmail.com   | \n    "); 
+	printf("|  2  |   Tran Van A   |   18  | 0339923143    | duy1238@gmail.com        | \n    ");
+	printf("|  1  | Doan Manh Duy  |   18  | 0336620883    | duy18namdinh@gmail.com   | \n    "); 
+	printf("|  2  |   Tran Van A   |   18  | 0339923143    | duy1238@gmail.com        | \n    "); 
+	printf("|  1  | Doan Manh Duy  |   18  | 0336620883    | duy18namdinh@gmail.com   | \n    "); 
+	printf("|  2  |   Tran Van A   |   18  | 0339923143    | duy1238@gmail.com        | \n    "); 
+	printf("|  1  | Doan Manh Duy  |   18  | 0336620883    | duy18namdinh@gmail.com   | \n    "); 
+	printf("|  2  |   Tran Van A   |   18  | 0339923143    | duy1238@gmail.com        | \n    "); 
+	printf("|  1  | Doan Manh Duy  |   18  | 0336620883    | duy18namdinh@gmail.com   | \n    "); 
+	printf("|  2  |   Tran Van A   |   18  | 0339923143    | duy1238@gmail.com        | \n    "); 
+	printf("|  1  | Doan Manh Duy  |   18  | 0336620883    | duy18namdinh@gmail.com   | \n    "); 
+	printf("|  2  |   Tran Van A   |   18  | 0339923143    | duy1238@gmail.com        | \n    ");  
+	
 
- #include <stdio.h>
-
-int main() {
-    char hovaten[50];
-    int tuoi;
-    char phone[20];
-    char email[50];
-
-    // Nhap ho va ten 
-    printf("nhap ho va ten: ");
-    fgets(hovaten, sizeof(hovaten), stdin);
-    printf("Nhap tuoi: ");
-    scanf("%d", &tuoi);
-    printf("Nhap phone: ");
-    scanf("%s",&phone); 
-    printf("Nh?p email c?a b?n: ");
-    scanf("%s",&email);
-
-    // In ra thông tin
-    printf(": %s",hovaten);
-    printf("S? tu?i: %d\n", tuoi);
-    printf("S? ði?n tho?i: %s\n",phone);
-    printf("Email: %s\n", email);
-
-    return 0;
-}
-
+return 0; 
+	
+	return 0; 
+} 
